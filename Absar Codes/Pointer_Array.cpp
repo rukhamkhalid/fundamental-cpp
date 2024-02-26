fe55b29ea2340bb0acc91d5de1c29a53a7ccc292
Absar Ahmed <absarahmed59@outlook.com>
2024-02-26 10:36:31 +0500

    Pointer Loop Increment

--- Absar Codes/Pointer_Array.cpp
+++ Absar Codes/Pointer_Array.cpp
@@ -4,7 +4,7 @@ using namespace std;
 int main(){
     int arr[3] = {1,2,3};
     int *ptr = arr;
-    for (int i=0; i<3;i++){
-        cout<<*(ptr+i)<<endl;
+    for (int i=0; i<3;i++,ptr++){
+        cout<<*ptr<<endl;
     }
 }
